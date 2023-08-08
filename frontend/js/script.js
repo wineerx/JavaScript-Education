35db720ccafe2e40178b87b71e46c2118661cbf6
winnerx <deusdamicrosoft@gmail.com>
2023-08-08 10:46:14 -0300

    feat: Basic Script

---
+++ frontend/js/script.js
@@ -0,0 +1,16 @@
+// Boolean
+// Numerico
+// String
+
+// Const - Constante, ou seja [NÃO MUDA NUNCA].  
+// Var   - Scopo global
+// Let   - Scopo Local
+
+
+
+var joao = 1 < 2 ? 'João' : 'Maria'; // Operador Ternário
+
+
+
+
+console.log(joao);
